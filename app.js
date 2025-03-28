4835d734baaeb8788258baf2b45d65322a70a98d
Omar AL AKOUM <omar.alakoum@outlook.com>
2025-03-28 13:50:45 +0200

    r

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ import cors from 'cors'
 const app = express();
 const port = process.env.port || 4000;
 
-app.use(cors({origin:'http://localhost:5173'})); // Url frontend
+// app.use(cors({origin:'http://localhost:5173'})); // Url frontend
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(users);
